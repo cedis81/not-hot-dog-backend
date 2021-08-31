2fbc6322b8d7a5316374537d00187c00486bacea
cedis81 <cedisweb0@gmail.com>
2021-08-31 18:21:23 -0400

    Update test database
    * Add hotdogs as a user field

--- server.js
+++ server.js
@@ -7,6 +7,33 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'john',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 3,
+      hotdogs: 4,
+      joined: new Date()
+    },
+    {
+      id: '124',
+      name: 'Sally',
+      email: 'Sally@gmail.com',
+      password: 'diu',
+      entries: 3,
+      hotdogs: 5,
+      joined: new Date()
+    }
+  ]
+}
+
+app.get('/', (req, res) => {
+  res.json(database.users)
+})
+
 
 app.listen(3000, ()=> {
   console.log('app is running on port 3000')

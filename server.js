e16f927ff2dcd8bceb8d6e2035a0c960028a56a7
cedis81 <cedisweb0@gmail.com>
2021-08-31 15:14:12 -0400

    Install cors package
    * Import and use cors package in server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require ('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('this is working');
-})
+app.use(bodyParser.json());
+app.use(cors());
+
 
 app.listen(3000, ()=> {
   console.log('app is running on port 3000')

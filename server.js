e95167c7c6af6475ada2dfd73885e77e6f48d59d
cedis81 <cedisweb0@gmail.com>
2021-09-07 18:26:06 -0400

    Create /image route
    * Create /image route as a put route
    * /image route, on success, increments user's entry count

--- server.js
+++ server.js
@@ -77,6 +77,15 @@ app.get('/profile/:id', (req, res) => {
     .catch(err => res.status(400).json('not found'))
 })
 
+app.put('/image' , (req, res) => {
+  const { id } = req.body;
+  db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => res.json(entries[0]))
+    .catch(err => res.status(400).json('unable to get entries'))
+})
 
 app.listen(3000, ()=> {
   console.log('app is running on port 3000')

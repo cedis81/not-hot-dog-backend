cf90b12bb92683e234e704055b1c1aad920cee7a
cedis81 <cedisweb0@gmail.com>
2021-09-08 17:12:08 -0400

    Refactor image route
    * Create image.js file in controllers folder to house route
    * Create handleImage function to be used as the callback in server.js
    * Export handleImage function from image.js
    * Require image from image.js in server.js
    * Pass db dependency to handleImage

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const knex = require('knex');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
+const image = require('./controllers/image');
 
 const db = knex({
   client: 'pg',
@@ -46,15 +47,7 @@ app.get('/profile/:id', (req, res) => {
     .catch(err => res.status(400).json('not found'))
 })
 
-app.put('/image' , (req, res) => {
-  const { id } = req.body;
-  db('users')
-    .where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => res.json(entries[0]))
-    .catch(err => res.status(400).json('unable to get entries'))
-})
+app.put('/image', (req,res) => {image.handleImage(req, res, db)})
 
 app.listen(3000, ()=> {
   console.log('app is running on port 3000')

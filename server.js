2b3bd9d33ce7fef9ec0487f7bea41006b16cb5e4
cedis81 <cedisweb0@gmail.com>
2021-09-08 17:28:42 -0400

    Refactor getProfile route
    * Create getProfile.js file in controllers folder to house route
    * Create handleGetProfile function to be used in server.js
    * Export handleGetProfile function from getProfile.js
    * Require getProfile from getProfile.js in server.js
    * Pass db dependency to handleGetProfile

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const knex = require('knex');
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const image = require('./controllers/image');
+const getProfile = require('./controllers/getProfile');
 
 const db = knex({
   client: 'pg',
@@ -25,29 +26,15 @@ app.use(cors());
 
 app.get('/', (req, res) => {
   res.json(database.users)
-})
+});
 
-app.post('/signin', (req, res) => {signin.handleSignIn(req, res, db, bcrypt)})
+app.post('/signin', (req, res) => {signin.handleSignIn(req, res, db, bcrypt)});
 
-app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
 
-app.get('/profile/:id', (req, res) => {
-  const { id } = req.params;
-  db('users')
-    .select('*')
-    .from('users')
-    .where({id})
-    .then(user => {
-        if(user.length) {
-          res.json(user[0])
-        } else {
-          res.status(400).json('not found')
-        }
-    })
-    .catch(err => res.status(400).json('not found'))
-})
+app.get('/profile/:id', (req, res) => {getProfile.handleGetProfile(req, res, db)});
 
-app.put('/image', (req,res) => {image.handleImage(req, res, db)})
+app.put('/image', (req,res) => {image.handleImage(req, res, db)});
 
 app.listen(3000, ()=> {
   console.log('app is running on port 3000')

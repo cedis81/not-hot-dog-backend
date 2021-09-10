b53745096e40226cd0604cb5168f00a97510c1da
cedis81 <cedisweb0@gmail.com>
2021-09-10 13:12:11 -0400

    Prevent blank form submission in signin.js
    * If user submits blank email or pw return error and end func
    * Destructure email and pw from req.body
    * Update json response to user[0] instead of user to connect front end

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,15 +1,19 @@
 const handleSignIn = (req, res, db, bcrypt) => {
+  const { email, password } = req.body;
+  if(!email || !password) {
+    return res.status(400).json('incorrect form submission');
+  }
   db.select('email', 'hash').from('login')
-    .where('email', '=', req.body.email)
+    .where('email', '=', email)
     .then(user => {
-      return bcrypt.compare(req.body.password, user[0].hash);
+      return bcrypt.compare(password, user[0].hash);
     })
     .then(user => {
       if (user) {
         return db.select('*').from('users')
-          .where('email', '=', req.body.email)
+          .where('email', '=', email)
           .then(user=> {
-            res.json(user)
+            res.json(user[0])
           })
           .catch(err=> res.status(400).json('error fetching user'))
       } else {

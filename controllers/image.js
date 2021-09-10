318674313fe632bb8cb7a4976113fb2f8e0ccc0e
cedis81 <cedisweb0@gmail.com>
2021-09-10 17:41:16 -0400

    Create imageurl route
    * Create imageurl route in server.js
    * Import ClarifaiStub in image.js
    * Create stub and metadata variables with temp Clarifai API Key
    * Create handleApiCall function in image.js to respond with API output
    * Export handleApiCall

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,26 @@
+const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
+
+const stub = ClarifaiStub.grpc();
+
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key b5c0cdb7d67d425bbae26da9597796d2");
+
+const handleApiCall = (req, res) => {
+  stub.PostModelOutputs(
+    {
+      model_id: "bd367be194cf45149e75f01d59f77ba7",
+      inputs: [{data: {image: {url: `${req.body.imageUrl}`}}}]
+    },
+    metadata,
+    (err, response) => {
+      if (err || response.status.code !== 10000) {
+        return res.status(400).json(`Received failed status: ${response.status.description}`);
+      }
+      res.json(response.outputs[0].data.concepts.filter(x=>x.name === 'hot dog')[0])
+    }
+  );
+}
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users')
@@ -9,5 +32,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-  handleImage
+  handleImage,
+  handleApiCall
 }
